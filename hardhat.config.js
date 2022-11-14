73fc3c2552a21ae6c99ce56bbfd16ebf6f4d43d8
Amit Mehta <amitmehta@Amits-MacBook-Air.local>
2022-11-14 16:46:39 +0530

    ERC20-contract

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+const INFURA_URL = process.env.INFURA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const YOUR_ETHERSCAN_API_KEY = process.env.YOUR_ETHERSCAN_API_KEY;
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    ropsten: {
+      url: INFURA_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    apiKey: YOUR_ETHERSCAN_API_KEY,
+  },
+};
